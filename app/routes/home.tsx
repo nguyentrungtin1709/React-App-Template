21f50c78ac9ce8b3e2d160ea51c0aefdd4240f41
Nguyễn Trung Tín <trungtincom7@gmail.com>
2025-03-12 23:37:56 +0700

    Remove some default components

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,14 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "React App Template" },
+    { name: "description", content: "Welcome to React App" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <></>
+  );
 }
